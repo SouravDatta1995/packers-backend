5c4a3af6447f28b2715530c393ee83cbf5d4914c
Sourav <souravdatta1995@gmail.com>
2021-05-15 19:26:06 +0530

    Updated UserController.java

--- src/main/java/com/packers/packersbackend/controller/UserController.java
+++ src/main/java/com/packers/packersbackend/controller/UserController.java
@@ -5,9 +5,11 @@ import com.packers.packersbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
@@ -16,6 +18,11 @@ public class UserController {
     @PostMapping("/")
     public User findUser(@RequestBody User user) {
         User result = userRepository.findById(user.getId()).get();
+        return result;
+    }
+    @PostMapping("/add")
+    public User addUser(@RequestBody User user) {
+        User result = userRepository.save(user);
         return result;
     }
 }
